430cf20aa9b0019843eab26701f45f7cdb7f7098
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-25 12:16:16 +0800

    Fixed bug : adding task using provider

--- lib/model/task_data.dart
+++ lib/model/task_data.dart
@@ -1,19 +1,25 @@
+import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/model/task.dart';
+// ignore_for_file: prefer_const_constructors
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Task 1'),
     Task(name: 'Task 2'),
     Task(name: 'Task 3'),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle) {
-    tasks.add(Task(name: newTaskTitle));
+    _tasks.add(Task(name: newTaskTitle));
     notifyListeners();
   }
 }
